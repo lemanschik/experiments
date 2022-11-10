4c3b9831acdd9ad1d4986e9ca595c14aeff5906f
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2022-11-10 10:56:06 +0100

    Update host.js

--- components/host/host.js
+++ components/host/host.js
@@ -4,4 +4,6 @@
 
 const host = (impl,context) => (context=impl());
 
-// .sys.runMicroTask
+// .sys.runMicroTask .fs .net use host.compile which implements stealify. and stealify uses sys
+// .process is importent it gets passed to impl() if we are not self the host as the other operating systems are process based.
+// or at last the interfaces that we use at present for them are process based.
