5476c6a24bebbfe3abbebce2497e5a41462cd0c0
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2022-11-10 16:00:56 +0100

    Update macos.js

--- components/installer/macos/macos.js
+++ components/installer/macos/macos.js
@@ -1,5 +1,9 @@
 if (window.location.href.indexOf('macos.html') > -1) {
-  document.body.innerHTML = `Router works`;
+  document.body.innerHTML = `
+  <h1>Install @stealify/host/macos</h1>
+  <p> Implements launch and watch for the macos operating system </p>
+  <h2>Install @stealify/manager/macos</h2>
+  <p> Component Scheduling Task Manager Integration Automator </p>`;
   console.log('Instantiated macos installer...')
 
   if ('serviceWorker' in navigator) {
