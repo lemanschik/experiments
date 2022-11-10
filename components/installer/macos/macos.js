3837c5545a21a49a15a160d99551d467bf1607e7
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2022-11-10 15:57:21 +0100

    Update macos.js

--- components/installer/macos/macos.js
+++ components/installer/macos/macos.js
@@ -1,21 +1,21 @@
 if (window.location.href.indexOf('macos.html') > -1) {
   document.body.innerHTML = `Router works`;
-} else {
-  window.location.href = new URL('./macos.html', window.location).href
-}
-  
-
-console.log('Instantiated macos installer...')
+  console.log('Instantiated macos installer...')
 
 if ('serviceWorker' in navigator) {
   // Register a service worker hosted at the root of the
   // site using the default scope.
   navigator.serviceWorker.register('./worker.js').then((registration) => {
     console.log('macos worker registration succeeded:', registration);
-    document.querySelector('github-only').remove()
   }, /*catch*/ (error) => {
     console.error(`Service worker registration failed: ${error}`);
   });
 } else {
   console.error('This browser is not supported.');
 }
+} else {
+  window.location.href = new URL('./macos.html', window.location).href
+}
+  
+
+
