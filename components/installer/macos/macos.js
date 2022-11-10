c6e123472e45d5f2ee4021d19963cc384027e720
Frank Lemanschik <frank@lemanschik.com>
2022-11-10 17:54:28 +0100

    Update macos.js

--- components/installer/macos/macos.js
+++ components/installer/macos/macos.js
@@ -11,6 +11,21 @@ if (window.location.href.indexOf('macos.html') > -1) {
     // site using the default scope.
     navigator.serviceWorker.register('./worker.js').then((registration) => {
       console.log('macos worker registration succeeded:', registration);
+      async function saveFile() {
+
+        // create a new handle
+        const newHandle = await window.showSaveFilePicker();
+
+        // create a FileSystemWritableFileStream to write to
+        const writableStream = await newHandle.createWritable();
+
+        // write our file
+        await writableStream.write("hi\0");
+
+        // close the file and write the contents to disk.
+        await writableStream.close();
+      }
+      saveFile()
     }, /*catch*/ (error) => {
       console.error(`Service worker registration failed: ${error}`);
     });
