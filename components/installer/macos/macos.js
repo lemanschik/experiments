d9a0d71ddfb685813d6262f5a6af4f80c7361c5c
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2022-11-10 15:58:01 +0100

    Update macos.js

--- components/installer/macos/macos.js
+++ components/installer/macos/macos.js
@@ -2,17 +2,17 @@ if (window.location.href.indexOf('macos.html') > -1) {
   document.body.innerHTML = `Router works`;
   console.log('Instantiated macos installer...')
 
-if ('serviceWorker' in navigator) {
-  // Register a service worker hosted at the root of the
-  // site using the default scope.
-  navigator.serviceWorker.register('./worker.js').then((registration) => {
-    console.log('macos worker registration succeeded:', registration);
-  }, /*catch*/ (error) => {
-    console.error(`Service worker registration failed: ${error}`);
-  });
-} else {
-  console.error('This browser is not supported.');
-}
+  if ('serviceWorker' in navigator) {
+    // Register a service worker hosted at the root of the
+    // site using the default scope.
+    navigator.serviceWorker.register('./worker.js').then((registration) => {
+      console.log('macos worker registration succeeded:', registration);
+    }, /*catch*/ (error) => {
+      console.error(`Service worker registration failed: ${error}`);
+    });
+  } else {
+    console.error('This browser is not supported.');
+  }
 } else {
   window.location.href = new URL('./macos.html', window.location).href
 }
