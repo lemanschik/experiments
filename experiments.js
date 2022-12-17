216fd1c5df8e6c4e7ed59a9e7359aa88c1f48842
Frank Lemanschik <frank@lemanschik.com>
2022-12-17 11:14:44 +0100

    Fix: typo in new operator of URL

--- experiments.js
+++ experiments.js
@@ -1,3 +1,3 @@
-export const resolve = await fetch('https://raw.githubusercontent.com/lemanschik/awesome/components/resolve/github.com.js').then(r=>r.blob()).then(blob=>new Blob([blob],{ type: "text/javascript" })).then(window.URL.createObjectURL).then(resolvedId=>import(resolvedId,nw URL(`https://raw.githubusercontent.com/lemanschik/awesome/components/resolve`)));
+export const resolve = await fetch('https://raw.githubusercontent.com/lemanschik/awesome/components/resolve/github.com.js').then(r=>r.blob()).then(blob=>new Blob([blob],{ type: "text/javascript" })).then(window.URL.createObjectURL).then(resolvedId=>import(resolvedId,new URL(`https://raw.githubusercontent.com/lemanschik/awesome/components/resolve`)));
 
 //await import(`https://raw.githubusercontent.com/lemanschik/awesome/components/resolve/github.com.js`);
